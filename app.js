a46907bbef71c72894f84b50a02eb63f0a40f7a7
I-A11 <d.islam1983@gmail.com>
2021-10-04 18:09:36 +1100

    adding JS file to the app

---
+++ app.js
@@ -0,0 +1,88 @@
+// Book Constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI Constructor
+function UI() {}
+
+// Add Book To List
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById("book-list");
+  // Create tr element
+  const row = document.createElement("tr");
+  // Insert cols
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X<a></td>
+  `;
+
+  list.appendChild(row);
+};
+
+//Show alert
+UI.prototype.showAlert = function (message, className) {
+  // Create div
+  const div = document.createElement("div");
+  // add classes
+  div.className = `alert ${className}`;
+  // Add text
+  div.appendChild(document.createTextNode(message));
+  // Get a perent
+  const container = document.querySelector(".container");
+  const form = document.querySelector("#book-form");
+  //Insertalert
+  container.insertBefore(div, form);
+
+  //Timeout after 3 sec
+  setTimeout(() => {
+    document.querySelector(".alert").remove();
+  }, 3000);
+};
+
+// Clear Fields
+UI.prototype.clearFields = function () {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+};
+
+// Event Listeners
+document.getElementById("book-form").addEventListener("submit", (e) => {
+  // Get form values
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
+
+  // Instantiate book
+  const book = new Book(title, author, isbn);
+
+  // Instantiate UI
+  const ui = new UI();
+
+  //Validatoion
+  if (title === "" || author === "" || isbn === "") {
+    ui.showAlert("Please fill in all fields", "error");
+  } else {
+    // Add book to list
+    ui.addBookToList(book);
+
+    // Clear fields
+    ui.clearFields();
+  }
+
+  // Add book to list
+  ui.addBookToList(book);
+
+  //show alert
+  ui.showAlert("Book Added", "success");
+
+  // Clear fields
+  ui.clearFields();
+
+  e.preventDefault();
+});
